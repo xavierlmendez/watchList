2f1aba2c6fef63edfe77459abacfe12b784a16ec
xaviermendez <xluis10.2@gmail.com>
2025-04-15 20:19:41 -0500

    adding spring boot start

---
+++ rest-service/src/main/java/com/watchlist/rest_service/Greeting.java
@@ -0,0 +1,3 @@
+package main.java.com.watchlist.rest_service;
+
+public record Greeting(long id, String content) { }
---
+++ rest-service/src/main/java/com/watchlist/rest_service/RestServiceApplication.java
@@ -0,0 +1,14 @@
+package com.watchlist.rest_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
